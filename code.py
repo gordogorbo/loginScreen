c9ed1f635044a1e4f18237897b495614f9ca8595
gordo <50698828+gordogorbo@users.noreply.github.com>
2020-07-31 13:04:44 +1000

    Create code.py

---
+++ code.py
@@ -0,0 +1,20 @@
+#importing tkinter
+from tkinter import *
+
+# initalizing tkinter
+top = Tk()
+top.geometry("400x250")
+
+# creating labels
+uname = Label(top, text = "username").place(x = 30, y = 50)
+password = Label(top, text = 'Password').place(x = 30, y = 90)
+
+# creating submit button
+sbmitbtn = Button(top, text = "Submit",activebackground = "pink",activeforeground = "blue").place(x = 30, y = 120)
+
+# creating entries
+e1 = Entry(top, width = 20).place(x = 100, y = 50)
+s2 = Entry(top, width = 20).place(x = 100, y = 90)
+
+# mainLoop
+top.mainloop()
