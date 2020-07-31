da45f046d9835304f49705e696727eecba698c5e
gordo <50698828+gordogorbo@users.noreply.github.com>
2020-07-31 13:35:33 +1000

    Update code.py

--- code.py
+++ code.py
@@ -6,11 +6,11 @@ top = Tk()
 top.geometry("400x250")
 
 # creating labels
-uname = Label(top, text = "username").place(x = 30, y = 50)
-password = Label(top, text = 'Password').place(x = 30, y = 90)
+uname = Label(top, text = "username").place(x = 50, y = 50)
+password = Label(top, text = 'Password').place(x = 50, y = 90)
 
 # creating submit button
-sbmitbtn = Button(top, text = "Submit",activebackground = "pink",activeforeground = "blue").place(x = 30, y = 120)
+sbmitbtn = Button(top, text = "Submit",activebackground = "pink",activeforeground = "blue").place(x = 50, y = 120)
 
 # creating entries
 e1 = Entry(top, width = 20).place(x = 100, y = 50)
